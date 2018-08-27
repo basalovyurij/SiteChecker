7fa5c03e4a625ddd3687bf162e194f53f1a52053
Basalov Yurij <basalov_yurij@mail.ru>
2018-08-27 22:51:59 +0300

    tests

---
+++ SiteChecker.Tests/SiteCheckManagerIntegrationTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestHacks;
+using SiteChecker.Logic.Models;
+using SiteChecker.Logic.Implementation;
+using System.Linq;
+using SiteChecker.Core;
+using System.Collections.Generic;
+using System.Net;
+
+namespace SiteChecker.Tests
+{
+    [TestClass]
+    public class SiteCheckManagerIntegrationTests : TestBase
+    {
+        private IEnumerable<SiteCheckInfo> TestCases
+        {
+            get
+            {
+                return new[]
+                {
+                    new SiteCheckInfo
+                    {
+                        Url = "https://ya.ru",
+                        IsAvaliable = true,
+                        StatusCode = HttpStatusCode.OK,
+                        ResponseTime = 1
+                    },
+                    new SiteCheckInfo
+                    {
+                        Url = "https://ya.ru/404",
+                        IsAvaliable = false,
+                        StatusCode = HttpStatusCode.NotFound,
+                        ResponseTime = 1
+                    },
+                    new SiteCheckInfo
+                    {
+                        Url = "http://ya321.ru/",
+                        IsAvaliable = false,
+                        StatusCode = null,
+                        ResponseTime = 1
+                    },
+                    new SiteCheckInfo
+                    {
+                        Url = "http://www.fakeresponse.com/api/?sleep=2",
+                        IsAvaliable = false,
+                        StatusCode = null,
+                        ResponseTime = null
+                    }
+                };
+            }
+        }
+
+        [TestMethod]
+        [DataSource("SiteChecker.Tests.SiteCheckManagerIntegrationTests.TestCases")]
+        public void ManagerTests()
+        {
+            var data = TestContext.GetRuntimeDataSourceObject<SiteCheckInfo>();
+
+            var manager = new SiteCheckManager(
+                new Config
+                {
+                    Urls = new List<string>() { data.Url },
+                    CheckTimeoutInMs = 1000,
+                    CheckIntervalInMs = 1000000
+                }, 
+                new SiteCheckProvider());
+            manager.ReCheck();
+            var res = manager.GetInfo().First();
+
+            Assert.AreEqual(res.IsAvaliable, data.IsAvaliable);
+            Assert.AreEqual(res.ResponseTime.HasValue, data.ResponseTime.HasValue);
+            Assert.AreEqual(res.StatusCode, data.StatusCode);
+        }
+    }
+}
---
+++ SiteChecker.Tests/SiteCheckProviderUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using SiteChecker.Logic.Models;
+using SiteChecker.Logic.Implementation;
+using MSTestHacks;
+using System.Net;
+
+namespace SiteChecker.Tests
+{
+    [TestClass]
+    public class SiteCheckProviderUnitTests : TestBase
+    {
+        private IEnumerable<SiteCheckInfo> TestCases
+        {
+            get
+            {
+                return new[]
+                {
+                    new SiteCheckInfo
+                    {
+                        IsAvaliable = true,
+                        StatusCode = HttpStatusCode.OK,
+                        ResponseTime = 1
+                    },
+                    new SiteCheckInfo
+                    {
+                        IsAvaliable = false,
+                        StatusCode = HttpStatusCode.NotFound,
+                        ResponseTime = 1
+                    },
+                    new SiteCheckInfo
+                    {
+                        IsAvaliable = false,
+                        StatusCode = null,
+                        ResponseTime = 1
+                    },
+                    new SiteCheckInfo
+                    {
+                        IsAvaliable = false,
+                        StatusCode = null,
+                        ResponseTime = null
+                    }
+                };
+            }
+        }
+
+        [TestMethod]
+        [DataSource("SiteChecker.Tests.SiteCheckProviderUnitTests.TestCases")]
+        public void ProviderTests()
+        {
+            var data = TestContext.GetRuntimeDataSourceObject<SiteCheckInfo>();
+
+            var res = new SiteCheckProvider().GetInfo(data.StatusCode, data.ResponseTime);
+
+            Assert.AreEqual(res.IsAvaliable, data.IsAvaliable);
+            Assert.AreEqual(res.ResponseTime, data.ResponseTime);
+            Assert.AreEqual(res.StatusCode, data.StatusCode);
+        }
+    }
+}
